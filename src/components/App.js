9ec86bce13a87e9e1bd1cf2a0e4e5f2a3096fd1a
eabra <emab21@student.bth.se>
2024-02-10 11:07:40 +0100

    Add Dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import Login from "./Login"
 import useLocalStorage from "../hooks/useLocalStorage"
+import Dashboard from "./Dashboard"
 
 function App() {
   const [id, setId] = useLocalStorage('id')
 
   return (
-    <>
-      <Login onSubmitId={setId} />
-    </>
+      id ? <Dashboard id={id} /> : <Login onSubmitId={setId} />
   )
 }
 
